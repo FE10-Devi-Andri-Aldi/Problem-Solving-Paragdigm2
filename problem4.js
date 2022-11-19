bf65841ab6b19a5b8c2d44cafedc0b6b12313923
Devi Andri Aldi <73923081+deviandrialdi@users.noreply.github.com>
2022-11-19 14:57:32 +0700

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -1,34 +1,35 @@
-const romawi = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-}
+function convertToRoman(num) {
+    let roman = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    };
 
-// -------------------------------- PROSES -------------------------------------------
-function romanNumerals(value) {
+
     let result = '';
-
-    for (key in romawi) { // key adalah index 
-        result += // result penampung hasil akhir
-            key.repeat(Math.floor(value / romawi[key]));
-        value %= romawi[key]; 
+  
+    for (let i of Object.keys(roman)) {
+      let angkaRomawi = Math.floor(num / roman[i]);
+      num -= angkaRomawi * roman[i];
+      result += i.repeat(angkaRomawi);
     }
+  
     return result;
-};
+  }
 
-// --------------------------------------- OUTPUT --------------------------------
-console.log(romanNumerals(6))    // VI
-console.log(romanNumerals(9))    // IX
-console.log(romanNumerals(23))   // XXIII
-console.log(romanNumerals(2021)) // MMXXI
-console.log(romanNumerals(1646)) // MDCXLVI
+  module.exports = romanNumerals;
+    // console.log(convertToRoman(6)) // VI
+    // console.log(convertToRoman(9))  // IX
+    // console.log(convertToRoman(23))   // XXIII
+    // console.log(convertToRoman(2021)) // MMXXI
+    // console.log(convertToRoman(1646)) // MDCXLVI
